72daec4f553be351ab2498076bcfeaa2597937c2
pusto <pusto@tongji.edu.cn>
2019-03-31 22:51:31 +0800

    now is 03312250

--- contiinteg.sh
+++ contiinteg.sh
@@ -3,16 +3,19 @@
 commit_msg=$1
 cur_dir=$(pwd)
 
+git add -A
+git commit -a -m $1
+
 if [ -d "$cur_dir/subtree0/" ];then
   echo "subtree already exists"
 else
   echo "exists nicht"
   git remote add subrepo0 https://github.com/pusto-tauranth/test_1
-  git subtree add -P subtree0 subrepo0 master
+  git subtree add -P subtree0 subrepo0 master --squash
 fi
 
 git fetch subrepo0 master
-git subtree pull -P subtree0 subrepo0 master
+git subtree pull -P subtree0 subrepo0 master --squash
 git add -A
-git commit -a -m $1
+git commit -a -m "$1"
 git push origin master
