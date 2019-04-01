f01760083aa69f46ce5c0cdf3f592b95bad24e9c
pusto <pusto@tongji.edu.cn>
2019-04-01 13:04:42 +0800

    autoCommitToAvoidSubtreePullError:WorkingTreeHasModifi

--- contiinteg.sh
+++ contiinteg.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sub1=(
+## TODO: add "subtree_name" "subrepo_name" "subrepo_branch_name" "subrepo_link" to this array
+subrepos=(
 "subtree0" "subrepo0" "master" "https://github.com/pusto-tauranth/test_1"
 "subtree1" "subrepo1" "master" "https://github.com/pusto-tauranth/pusto-mathe-kit"
 )
@@ -10,44 +11,30 @@ commit_msg=$1
 
 cur_dir=$(pwd)
 
-for((i=0;i<${#sub1[@]}/4;i++));do
+for((i=0;i<${#subrepos[@]}/4;i++));do
 
-if [ -d "$cur_dir/${sub1[((i*4+0))]}/" ];then
-  echo "Subtree ${sub1[((i*4+0))]} already exists."
-else
-  echo "Subtree ${sub1[((i*4+0))]} does not exist so add it now."
-  git remote add ${sub1[((i*4+1))]} ${sub1[((i*4+3))]}
-  git subtree add -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash
-fi
+  if [ -d "$cur_dir/${subrepos[((i*4+0))]}/" ];then
+    echo "ContinuousIntegration Info: Subtree ${subrepos[((i*4+0))]} already exists."
+  else
+    echo "ContinuousIntegration Info: Subtree ${subrepos[((i*4+0))]} does not exist so add it now."
+    git remote add ${subrepos[((i*4+1))]} ${subrepos[((i*4+3))]}
+    git subtree add -P ${subrepos[((i*4+0))]} ${subrepos[((i*4+1))]} ${subrepos[((i*4+2))]} --squash
+  fi
 
-git fetch ${sub1[((i*4+1))]} ${sub1[((i*4+2))]}
-git subtree pull -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash -m "subtree_pull_${sub1[((i*4+0))]}"
-subt_pull_exit=$?
-echo "la exit status'est $subt_pull_exit"
-if [ $subt_pull_exit -ne 0 ];then
-## Following 2 lines are to avoid subtree pull Error: "Working tree has modifications. Cannot add."
-git add -A
-git commit -a -m "autoCommitToAvoidSubtreePullError:WorkingTreeHasModifi" #--allow-empty
-#git reset --soft HEAD~1
-git fetch ${sub1[((i*4+1))]} ${sub1[((i*4+2))]}
-git subtree pull -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash -m "subtree_pull_${sub1[((i*4+0))]}"
-fi
+  git fetch ${subrepos[((i*4+1))]} ${subrepos[((i*4+2))]}
+  git subtree pull -P ${subrepos[((i*4+0))]} ${subrepos[((i*4+1))]} ${subrepos[((i*4+2))]} --squash -m "subtree_pull_${subrepos[((i*4+0))]}"
+  subt_pull_exit=$?
+  echo "la exit status'est $subt_pull_exit"
+  if [ $subt_pull_exit -ne 0 ];then
+    ## Following 2 lines are to avoid subtree pull Error: "Working tree has modifications. Cannot add."
+    git add -A
+    git commit -a -m "autoCommitToAvoidSubtreePullError:WorkingTreeHasModifi" #--allow-empty
+
+    git fetch ${subrepos[((i*4+1))]} ${subrepos[((i*4+2))]}
+    git subtree pull -P ${subrepos[((i*4+0))]} ${subrepos[((i*4+1))]} ${subrepos[((i*4+2))]} --squash -m "subtree_pull_${subrepos[((i*4+0))]}"
+  fi
 
 done
-
-#if [ -d "$cur_dir/subtree0/" ];then
-#  echo "Subtree already exists."
-#else
-#  echo "Subtree does not exist so add it now."
-#  git remote add subrepo0 https://github.com/pusto-tauranth/test_1
-#  git subtree add -P subtree0 subrepo0 master --squash
-#fi
-
-#git fetch subrepo0 master
-#git subtree pull -P subtree0 subrepo0 master --squash
-
-## Following 1 line is to prevent the aforementioned "git mommit" and "git subtree pull" from adding commits to repo, which make the commit record of the repo ungraceful.
-
 
 
 ## According to https://www.gnu.org/software/make/manual/html_node/Running.html#Running , the exit status is not 0 when make is not fully successful, hence the failure check is as below who will push only when make succeeded. 
