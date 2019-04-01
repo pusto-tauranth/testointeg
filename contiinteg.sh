f973577d5ac6f0ef01db63e0889b02f2cba4ebd2
pusto <pusto@tongji.edu.cn>
2019-04-01 11:31:54 +0800

    toAvoidSubtreePullError:WorkingTreeHasModifi

--- contiinteg.sh
+++ contiinteg.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-count=1
 #sub="subtree0 subrepo0 master https://github.com/pusto-tauranth/test_1""subtree1 subrepo1 master https://github.com/pusto-tauranth/pusto-mathe-kit"
 sub1=(
 "subtree0" "subrepo0" "master" "https://github.com/pusto-tauranth/test_1"
 "subtree1" "subrepo1" "master" "https://github.com/pusto-tauranth/pusto-mathe-kit"
 )
+((count=${#sub1[@]}/4+1))
 
 ## $1 should be your comment
 commit_msg=$1
@@ -24,7 +24,7 @@ else
   git remote add ${sub1[((i*4+1))]} ${sub1[((i*4+3))]}
   git subtree add -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash
 fi
-((count+=1))
+
 git fetch ${sub1[((i*4+1))]} ${sub1[((i*4+2))]}
 git subtree pull -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash
 done
@@ -40,7 +40,7 @@ done
 #git subtree pull -P subtree0 subrepo0 master --squash
 
 ## Following 1 line is to prevent the aforementioned "git mommit" and "git subtree pull" from adding commits to repo, which make the commit record of the repo ungraceful.
-git reset --soft HEAD~3
+#git reset --soft HEAD~3
 
 
 ## According to https://www.gnu.org/software/make/manual/html_node/Running.html#Running , the exit status is not 0 when make is not fully successful, hence the failure check is as below who will push only when make succeeded. 
