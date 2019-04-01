991b896d0946de5215f3e3dd2810b38c991ecbb6
pusto <pusto@tongji.edu.cn>
2019-04-01 09:44:23 +0800

    nowis04010956

--- contiinteg.sh
+++ contiinteg.sh
@@ -40,7 +40,7 @@ done
 #git subtree pull -P subtree0 subrepo0 master --squash
 
 ## Following 1 line is to prevent the aforementioned "git mommit" and "git subtree pull" from adding commits to repo, which make the commit record of the repo ungraceful.
-#git reset --soft HEAD~$count
+git reset --soft HEAD~3
 
 
 ## According to https://www.gnu.org/software/make/manual/html_node/Running.html#Running , the exit status is not 0 when make is not fully successful, hence the failure check is as below who will push only when make succeeded. 
