4d8f75d4f033f3cc57646ef034b4eb0455344d52
pusto <pusto@tongji.edu.cn>
2019-04-01 12:36:18 +0800

    nowis04011236

--- contiinteg.sh
+++ contiinteg.sh
@@ -13,7 +13,7 @@ commit_msg=$1
 ## Following 2 lines are to avoid subtree pull Error: "Working tree has modifications. Cannot add."
 git add -A
 git commit -a -m "toAvoidSubtreePullError:WorkingTreeHasModifi" --allow-empty
-
+git reset --soft HEAD~1
 cur_dir=$(pwd)
 
 for((i=0;i<${#sub1[@]}/4;i++));do
@@ -26,7 +26,7 @@ else
 fi
 
 git fetch ${sub1[((i*4+1))]} ${sub1[((i*4+2))]}
-git subtree pull -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash
+git subtree pull -P ${sub1[((i*4+0))]} ${sub1[((i*4+1))]} ${sub1[((i*4+2))]} --squash -m "subtree_pull_${sub1[((i*4+0))]}"
 echo "la exit status'est $?"
 done
 #if [ -d "$cur_dir/subtree0/" ];then
@@ -41,7 +41,7 @@ done
 #git subtree pull -P subtree0 subrepo0 master --squash
 
 ## Following 1 line is to prevent the aforementioned "git mommit" and "git subtree pull" from adding commits to repo, which make the commit record of the repo ungraceful.
-#git reset --soft HEAD~3
+
 
 
 ## According to https://www.gnu.org/software/make/manual/html_node/Running.html#Running , the exit status is not 0 when make is not fully successful, hence the failure check is as below who will push only when make succeeded. 
